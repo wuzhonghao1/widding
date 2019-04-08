9a2eb836db27dcd09fa3b34190a4dbaaf1cadc56
ZhonghaoWu <wuzh3@asiainfo.com>
2019-04-08 13:54:08 +0800

    轮播图

--- src/component/centermode.js
+++ src/component/centermode.js
@@ -1,42 +1,33 @@
 import React, { Component } from "react";
-import Slider from "react-slick";
-// import "~slick-carousel/slick/slick.css";
-// import "~slick-carousel/slick/slick-theme.css";
-// import "../node_modules/slick-carousel/slick/slick.css";
-// import "../node_modules/slick-carousel/slick/slick-theme.css";
-
-class CenterMode extends React.Component{
-  render() {
+import { Carousel } from 'antd';
+import "../../node_modules/slick-carousel/slick/slick.css"
+import "../../node_modules/slick-carousel/slick/slick-theme.css"
+import "../static/css/slick.css" 
+import "../static/css/slick-theme.css" 
+import "../static/css/docs.css" 
+class CenterMode extends React.Component {
+  onChange(a, b, c) {
+    console.log(a, b, c);
+  }
+  
+  render(){
     const settings = {
       className: "center",
       centerMode: true,
       infinite: true,
       centerPadding: "60px",
       slidesToShow: 3,
-      speed: 500
+      speed: 500,
+      autoplay:true
     };
     return (
-        <Slider {...settings}>
-            <div>
-              <h3>1</h3>
-            </div>
-            <div>
-              <h3>2</h3>
-            </div>
-            <div>
-              <h3>3</h3>
-            </div>
-            <div>
-              <h3>4</h3>
-            </div>
-            <div>
-              <h3>5</h3>
-            </div>
-            <div>
-              <h3>6</h3>
-            </div>
-      </Slider>
-    );
+      <Carousel afterChange={this.onChange} {...settings}>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+      </Carousel> 
+    )
   }
 }
 export default CenterMode;
